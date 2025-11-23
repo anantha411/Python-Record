228c0645571df1bb88c785595b36cf9d6bef8f3f
anantha411 <11249a411@kanchiuniv.ac.in>
2025-11-23 23:28:16 +0530

    Create starpattendisplay.py

---
+++ starpattendisplay.py
@@ -0,0 +1,10 @@
+print("Star Pattern Display")
+num = 7
+x = num
+for i in range(1, 6, 1):
+    num = num - 1
+    for j in range(1, num, 1):
+        print("*", end=" ")
+    x = num - 1
+    print()
+print("End of Program")
